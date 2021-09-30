940a26c5aa36bc26131d88bc72df3ad83c1e095d
GregWasz <gregory.waszkow@icloud.com>
2021-09-30 18:26:14 +0200

    Added Database connection

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 
+from db.database import engine
+from routers import users
+from db import database
+
+database.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+app.include_router(users.user_router, prefix='/users', tags=['users'])
 
 
 @app.get("/")
