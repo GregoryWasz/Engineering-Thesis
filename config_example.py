c45724ba8eb0742cb76bda04bf936b11b51a23a3
GregWasz <gregory.waszkow@icloud.com>
2022-01-16 14:34:29 +0100

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,2 +1,5 @@
 # Change name to config.py
 DATABASE_URL = 'Your database url'
+SECRET_KEY = 'Your secret key'
+ALGORITHM = 'Hashing algorithm for JWT'
+ACCESS_TOKEN_EXPIRE_MINUTES = 'Your token access expire time in minutes -> provide type int for this variable'
